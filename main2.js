355590d2f9cffe9dbef25452241d3b4164fbf0b6
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-01-20 22:59:40 +0900

    메인화면 2번

--- main2.js
+++ main2.js
@@ -1,53 +1,76 @@
 const listMenu = document.querySelectorAll('.listMenu');
 const message = document.querySelector('.message');
+const videoTag = document.querySelector('.video');
+
 
 const MY_API= '7c7a2e8a5fba7053cbf279ba513e69da';
 let myLatitude, myLongitude;
 
-navigator.geolocation.getCurrentPosition(function(pos){
-    myLatitude=pos.coords.latitude;
-    myLongitude=pos.coords.longitude;
-})
+// 경도 위도 가져오는 함수
+// navigator.geolocation.getCurrentPosition(function(pos){
+//     myLatitude=pos.coords.latitude;
+//     myLongitude=pos.coords.longitude;
+// })
 
 
 
-function handleMouseIn(event){
-    const parent = event.target.parentNode;
-    console.log(parent);
-    const subMenu=event.target.children[1];
-    const innerTxt=event.target.children[0];
-    subMenu.classList.remove('displayNone');
-    innerTxt.style.backgroundColor='#f6f9fc';
-    parent.style.backgroundColor='snow';
+function handleMenuIn(event){
+    const parentMenu=event.target.parentElement;
+    const smallMenu = event.target.children[1];
+    const bigMenu=event.target.children[0];
+    parentMenu.style.backgroundColor="#f6f9fc";
+    parentMenu.style.borderBottom="solid 1px";
+    bigMenu.style.color="black";
+    bigMenu.style.backgroundColor="rgb(245,245,245)";
+    smallMenu.classList.remove('displayNone');
 }
 
-function handleMouseOut(event){
-    const parent = event.target.parentNode;
-    console.log(parent);
-    const subMenu=event.target.children[1];
-    const innerTxt=event.target.children[0];
-    subMenu.classList.add('displayNone');
-    parent.style.backgroundColor='transparent';
-    innerTxt.style.backgroundColor=white;
+function handleMenuOut(event){
+    const parentMenu=event.target.parentElement;
+    const smallMenu = event.target.children[1];
+    const bigMenu=event.target.children[0];
+    parentMenu.style.backgroundColor="transparent";
+    parentMenu.style.borderBottom="none";
+    smallMenu.classList.add('displayNone');
+    bigMenu.style.color="gray";
+    bigMenu.style.backgroundColor="transparent";
 }
 
 function displayMessage(){
-    let ranNum = parseInt(Math.random()*1000);
+    let ranNum = parseInt(Math.random()*100);
     let innerMessage='';
     if (ranNum<10){
-        innerMessage=`00${ranNum}`;
+        innerMessage=`${ranNum}0`;
     } else if(ranNum<100){
-        innerMessage=`0${ranNum}`;
+        innerMessage=`${ranNum}`;
     } else {
         innerMessage=`${ranNum}`;
     }
-    message.innerHTML=`지금 주변에 ${innerMessage} 명의 회원들이 여행 메이트를 찾고 있습니다!!`;
+    message.innerHTML=`지금 주변에 ${innerMessage}명의 회원들이 여행 메이트를 찾고 있습니다!!`;
 }
 
+function handleListIn(event){
+    const innerList = event.target;
+    innerList.style.color="orange";
+}
+
+function handleListOut(event){
+    const innerList = event.target;
+    innerList.style.color="black";
+}
 
 listMenu.forEach(element =>{
-    element.addEventListener("mouseenter", handleMouseIn);
-    element.addEventListener("mouseleave", handleMouseOut);
+    element.addEventListener("mouseenter", handleMenuIn);
+    element.addEventListener("mouseleave", handleMenuOut);
 })
+
+listMenu.forEach(element=>{
+    const listElement=element.querySelectorAll('li');
+    listElement.forEach(li=>{
+        li.addEventListener("mouseenter", handleListIn);
+        li.addEventListener("mouseleave", handleListOut);
+    })
+})
+
 setInterval(displayMessage, 1000);
 
