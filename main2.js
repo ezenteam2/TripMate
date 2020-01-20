fc9c137d3d83705d4528dfbe6df6aa0f0fded611
dlgudwnsgpffh <59990737+dlgudwnsgpffh@users.noreply.github.com>
2020-01-21 00:41:39 +0900

    메뉴 애니메이션 추가

--- main2.js
+++ main2.js
@@ -21,8 +21,9 @@ function handleMenuIn(event){
     parentMenu.style.backgroundColor="#f6f9fc";
     parentMenu.style.borderBottom="solid 1px";
     bigMenu.style.color="black";
+    smallMenu.style.animation="1s fadeIn";
+    smallMenu.style.animationFillMode="forwards";
     bigMenu.style.backgroundColor="rgb(245,245,245)";
-    smallMenu.classList.remove('displayNone');
 }
 
 function handleMenuOut(event){
@@ -31,7 +32,8 @@ function handleMenuOut(event){
     const bigMenu=event.target.children[0];
     parentMenu.style.backgroundColor="transparent";
     parentMenu.style.borderBottom="none";
-    smallMenu.classList.add('displayNone');
+    smallMenu.style.animation="0s fadeOut";
+    smallMenu.style.animationFillMode="backwards";
     bigMenu.style.color="gray";
     bigMenu.style.backgroundColor="transparent";
 }
